be06b661aec6798bec067554944113fe2130e084
Noxy <107372460+noxygalaxy@users.noreply.github.com>
2026-01-17 02:37:54 +0100

    Create install-requirements.bat

---
+++ Installation/install-requirements.bat
@@ -0,0 +1,13 @@
+@echo off
+REM Install FFmpeg via winget
+winget install -e --id Gyan.FFmpeg
+
+REM Install Python packages
+pip install plyer
+pip install pillow
+pip install ffmpeg
+pip install cairosvg
+
+echo.
+echo Done. You can close this window now.
+pause
